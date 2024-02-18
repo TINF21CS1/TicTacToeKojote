7880e73d2b533ed2fa1b70c031b6c63e513e4513
Julian Lemmerich <41118534+JM-Lemmi@users.noreply.github.com>
2024-02-18 13:22:16 +0100

    remove `current_player` from GameState

--- Game/gamestate.py
+++ Game/gamestate.py
@@ -4,7 +4,6 @@ class GameState:
 
     Attributes:
         _playfield (list[list[int]]): The playfield of the game, represented as a 2D list.
-        _current_player (int): The current player's number.
         _finished (bool): Indicates whether the game has finished.
         _winner (int): The number of the winning player, if any.
 
@@ -14,26 +13,22 @@ class GameState:
         set_winner(self, winner: int): Sets the winner of the game.
         winner(self) -> int: Returns the number of the winning player.
         finished(self) -> bool: Returns True if the game has finished, False otherwise.
-        current_player(self) -> int: Returns the number of the current player.
         playfield(self) -> list[list[int]]: Returns the playfield of the game.
         playfield_value(self, position: (int, int)) -> int: Returns the value at the specified position on the playfield.
     """
 
     _playfield: [int][int]
-    _current_player: int
     _finished: bool
     _winner: int
 
-    def __init__(self, starting_player: int, playfiled_dimensions: (int, int) = (3,3)):
+    def __init__(self, playfiled_dimensions: (int, int) = (3,3)):
         """
         Initializes a new instance of the GameState class.
 
         Args:
-            starting_player (int): ID of the player who starts the game.
             playfiled_dimensions (tuple[int, int], optional): The dimensions of the playfield. Defaults to (3, 3).
         """
         self._playfield = [[0 for _ in range(playfiled_dimensions[0])] for _ in range(playfiled_dimensions[1])]
-        self._current_player = starting_player
         self._finished = False
         self._winner = 0
     
@@ -77,16 +72,6 @@ class GameState:
         """
         return self._finished
     
-    @property
-    def current_player(self) -> int:
-        """
-        Returns the id of the current player.
-
-        Returns:
-            int: The id of the current player.
-        """
-        return self._current_player
-
     @property
     def playfield(self) -> [int][int]:
         """
