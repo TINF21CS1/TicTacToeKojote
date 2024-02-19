4427df70ad033bb0e244de8f5a0492308fe5033b
Hauke Platte <platte@stulz-digital-solutions.com>
2024-02-19 14:55:28 +0100

    feat(Lobby-rework): redesigned Lobby for more options [TTTK-11, TTTK-13]

--- UI/lobby.py
+++ UI/lobby.py
@@ -1,65 +1,193 @@
 import tkinter as tk
+import time
+
 from .base_frame import base_frame
 from .field_frame import Field
 from .network import get_connected_player
+
+class playerFrame(tk.Frame):
+    def __init__(self, master):
+        super().__init__(master)
+        self._create_base_widgets()
+        self._display_base_widgets()
+        self._start(set=False)
+
+    def _create_base_widgets(self):
+        self.btnStart = tk.Button(self, text="Start", command=self._start)
+    
+    def _display_base_widgets(self):
+        self.btnStart.grid(column=1, row=100, sticky=tk.E+tk.W)
+
+    def _start(self, set=None):
+        if(set != None):
+            new_state = self.master.master.start(self.master.player_id, set)
+        else:
+            new_state = self.master.master.start(self.master.player_id)
+        if(new_state == True):
+            self.btnStart.config(text="Ready!")
+        else:
+            self.btnStart.config(text="Start")
+
+class localPlayerFrame(playerFrame):
+    def __init__(self, master):
+        super().__init__(master)
+        self._create_widgets()
+        self._display_widgets()
+        self._check_valid_selection()
+
+    def _create_widgets(self):
+        self.lblControl = tk.Label(self, text="Control")
+        self.varControl = tk.StringVar(self, value="Select")
+        self.varControl.trace_add("write", lambda *args: self._control_change())
+        self.optControl = tk.OptionMenu(self, self.varControl, "Mouse", "QEYC", "IP,-")
+        self.lblProfile = tk.Label(self, text="Profile")
+        self.varProfile = tk.StringVar(self, value="Select")
+        self.varProfile.trace_add("write", lambda *args: self._profile_change())
+        self.optProfile = tk.OptionMenu(self, self.varProfile, "Profile 1", "Profile 2", "Profile 3")
+
+    def _display_widgets(self):
+        #self.columnconfigure([0,1], weight=1)
+        self.lblControl.grid(column=0, row=10, sticky=tk.E+tk.W)
+        self.optControl.grid(column=1, row=10, sticky=tk.E+tk.W)
+        self.lblProfile.grid(column=0, row=11, sticky=tk.E+tk.W)
+        self.optProfile.grid(column=1, row=11, sticky=tk.E+tk.W)
+
+    def _control_change(self):
+        self._check_valid_selection()
+
+    def _profile_change(self):
+        self._check_valid_selection()
+
+    def _check_valid_selection(self):
+        if(self.varControl.get() == "Select" or self.varProfile.get() == "Select"):
+            self.btnStart.config(state=tk.DISABLED)
+        else:
+            self.btnStart.config(state=tk.NORMAL)
+
+class AIPlayerFrame(playerFrame):
+    def __init__(self, master):
+        super().__init__(master)
+        self._create_widgets()
+        self._display_widgets()
+
+    def _create_widgets(self):
+        self.lblStrength = tk.Label(self, text="Strength")
+        self.varStrength = tk.StringVar(self, value="Select")
+        self.varStrength.trace_add("write", lambda *args: self._strength_change())
+        self.optStrength = tk.OptionMenu(self, self.varStrength, "Easy", "Medium", "Expert")
+
+    def _display_widgets(self):
+        #self.columnconfigure([0,1], weight=1)
+        self.lblStrength.grid(column=0, row=1, sticky=tk.E+tk.W)
+        self.optStrength.grid(column=1, row=1, sticky=tk.E+tk.W)
+        self.btnStart.grid_forget()
+
+    def _strength_change(self):
+        if(self.varStrength.get() == "Select"):
+            self._start(False)
+        else:
+            self._start(True)
+
+class NetworkPlayerFrame(playerFrame):
+    def __init__(self, master):
+        super().__init__(master)
+        self._create_widgets()
+        self._display_widgets()
+
+    def _create_widgets(self):
+        self.lblStrength = tk.Label(self, text="Strength")
+        self.optStrength = tk.OptionMenu(self, tk.StringVar(), "Easy", "Medium", "Expert")
+
+    def _display_widgets(self):
+        #self.columnconfigure([0,1], weight=1)
+        self.lblStrength.grid(column=0, row=1, sticky=tk.E+tk.W)
+        self.optStrength.grid(column=1, row=1, sticky=tk.E+tk.W)
+
+class playerdisplay(tk.Frame):
+    def __init__(self, master, player):
+        super().__init__(master)
+        self.player_id = player
+        self._create_widgets()
+        self._display_widgets()
+
+    def _create_widgets(self):
+        self.lblPlayer = tk.Label(self, text=f'Player {self.player_id+1}')
+        self.option = tk.StringVar()
+        self.option.trace_add("write", lambda *args: self.type_change())
+        self.option.set("Please select")
+        self.lblType = tk.OptionMenu(self, self.option, "Local", "AI", "Network")
+        self.playerOptions = tk.Frame(self)
+
+    def _display_widgets(self):
+        self.columnconfigure(0, weight=1)
+        self.lblPlayer.grid(column=0, row=0, sticky=tk.E+tk.W)
+        self.lblType.grid(column=0, row=1, sticky=tk.E+tk.W)
+        self.playerOptions.grid(column=0, row=2, sticky=tk.E+tk.W)
+
+    def type_change(self):
+        try:self.playerOptions.destroy()
+        except:pass
+        match self.option.get():
+            case "Local":
+                self.playerOptions = localPlayerFrame(self)
+            case "AI":
+                self.playerOptions = AIPlayerFrame(self)
+            case "Network":
+                pass
+            case _:
+                self.playerOptions = tk.Frame(self)
+        self.playerOptions.grid(column=0, row=2, sticky=tk.E+tk.W)
 
 class Lobby(base_frame):
     def __init__(self, master):
         super().__init__(master)
         self.player = list()
+        self.player_ready = [False]*2
+        self.ready = tk.BooleanVar(self, value=False)
+        self.after_handles = list()
+        self.ready.trace_add("write", lambda *args: self._timer_trigger())
         self._create_widgets()
         self._display_widgets()
 
     def _create_widgets(self):
         self.lblheading = tk.Label(self, width=20, height=5, bg="white", text="Lobby")
-        self.btnJoin = tk.Button(self, width=20, height=5, text="Join Lobby", command=self.select_join)
-        self.lblAddrHeading = tk.Label(self, text="Address")
-        self.inputLobby =  tk.Entry(self)
-        self.btnConnect = tk.Button(self, text="Connect", command=self.connect)
-        self.btnCreate = tk.Button(self, width=20, height=5, text="Create Lobby", command=self.select_create)
-        self.lblAddr = tk.Label(self, text="Connect to your local address")
-        self.btnBack = tk.Button(self, text="Back", width=20, height=5, command=self.master.show_menu)
+        self.lbllocation = tk.Label(self, text="at local")
+        self.playerType = [None]*2
+        self.playerType[0] = playerdisplay(self, 0)
+        self.playerType[1] = playerdisplay(self, 1)
+        self.lblTimer = tk.Label(self, text="")
 
     def _display_widgets(self):
-        self.lblheading.grid(column=0, row=0, columnspan=3)
-        self.btnJoin.grid(column=0, row=1, columnspan=3)
-        self.btnCreate.grid(column=0, row=30, columnspan=3)
+        self.columnconfigure([0,10], weight=10)
+        self.columnconfigure(5, weight=1)
+        self.lblheading.grid(column=0, row=0, columnspan=11, sticky=tk.E+tk.W)
+        self.lbllocation.grid(column=0, row=1, columnspan=11, sticky=tk.E+tk.W)
+        self.playerType[0].grid(column=0, row=2, sticky=tk.N+tk.E+tk.W)
+        self.playerType[1].grid(column=10, row=2, sticky=tk.N+tk.E+tk.W)
+        self.lblTimer.grid(column=0, row=100, columnspan=11, sticky=tk.E+tk.W)
 
-        self.btnBack.grid(column=0, row=50, columnspan=3)
+    def start(self, player, set=None):
+        if(set != None):
+            self.player_ready[player] = set
+        else:
+            self.player_ready[player] = not self.player_ready[player]
+        if(self.player_ready[0] == self.player_ready[1] == True):
+            self.ready.set(True)
+        else:
+            self.ready.set(False)
+        return self.player_ready[player]
 
-    def select_join(self):
-        self.lblAddr.grid_forget()
-        for e in self.player:
-            e[0].grid_forget()
-            e[1].grid_forget()
-        self.player = []
+    def _timer_trigger(self):
+        if(self.ready.get() == True):
+            self._timer(5)
+        else:
+            self.lblTimer.config(text="")
+            if(len(self.after_handles) > 0):
+                self.after_cancel(self.after_handles.pop())
 
-        self.lblAddrHeading.grid(column=0, row=2)
-        self.inputLobby.grid(column=1, row=2)
-        self.btnConnect.grid(column=2, row=2)
-
-    def select_create(self):
-        self.lblAddrHeading.grid_forget()
-        self.inputLobby.grid_forget()
-        self.btnConnect.grid_forget()
-
-        for e in self.player:
-            e[0].grid_forget()
-            e[1].grid_forget()
-        self.player = []
-
-        self.lblAddr.grid(column=0, row=31, columnspan=3)
-        for e in get_connected_player():
-            self.player.append([tk.Label(self, text=e), tk.Button(self, text="Start", command=lambda f=e: self.start(f))])
-        self.player = self.player[0:10]
-        for i, e in enumerate(self.player):
-            e[0].grid(column=0, row=32+i, columnspan=2)
-            e[1].grid(column=2, row=32+i)
-
-    def connect(self):
-        #do some network shit
-        self.master.show(Field)
-
-    def start(self, player):
-        #do some network shit
-        self.master.show(Field)
+    def _timer(self, counter):
+        self.lblTimer.config(text=f'Game starts in {counter}...')
+        if(counter == 0):
+            self.master.show(Field)
+        elif(counter > 0):
+            self.after_handles.append(self.after(1000, self._timer, counter -1))
--- UI/menu.py
+++ UI/menu.py
@@ -9,19 +9,39 @@ class Menu(base_frame):
         super().__init__(master)
         self._create_widgets()
         self._display_widgets()
+        self.address_toogle = False
 
     def _create_widgets(self):
         
-        self.btnField = tk.Button(self, width=20, height=5, text='Play', command=lambda: self.master.show(Field))
-        self.btnMulti = tk.Button(self, width=20, height=5, text='Multiplayer', command=lambda: self.master.show(Lobby))
+        self.btnField = tk.Button(self, width=20, height=5, text='Play / Start Lobby', command=lambda: self.master.show(Lobby))
+        self.btnMulti = tk.Button(self, width=20, height=5, text='Connect to Lobby', command=lambda: self._toogle_address(state=True))
         self.btnStats = tk.Button(self, width=20, height=5, text='Statistics')#, command=lambda: self.master.show(Statistics))
         self.btnExit = tk.Button(self, width=20, height=5, text='Exit', command=lambda: self.master.destroy())
+
+        self.lblAddr = tk.Label(self, text="Address")
+        self.inputLobby =  tk.Entry(self)
+        self.btnConnect = tk.Button(self, text="Connect", command=lambda: self.master.show(Lobby))
         
 
     def _display_widgets(self):
 
+        self.columnconfigure([0,10], weight=1)
         # display the buttons created in the _create_widgets method
-        self.btnField.pack(fill=tk.Y, expand=True)
-        self.btnMulti.pack(fill=tk.Y, expand=True)
-        self.btnStats.pack(fill=tk.Y, expand=True)
-        self.btnExit.pack(fill=tk.Y, expand=True)
+        self.btnField.grid(sticky=tk.E+tk.W, column=3, row=0, columnspan=5)#pack(fill=tk.Y, expand=True)
+        self.btnMulti.grid(sticky=tk.E+tk.W, column=3, row=1, columnspan=5)#pack(fill=tk.Y, expand=True)
+        self.btnStats.grid(sticky=tk.E+tk.W, column=3, row=3, columnspan=5)#pack(fill=tk.Y, expand=True)
+        self.btnExit.grid(sticky=tk.E+tk.W, column=3, row=4, columnspan=5)#pack(fill=tk.Y, expand=True)
+
+    def _toogle_address(self, state=None):
+        if state == None:
+            state = not self.address_toogle
+        if state != self.address_toogle:
+            if state:
+                self.lblAddr.grid(sticky=tk.E+tk.W, column=4, row=2)
+                self.inputLobby.grid(sticky=tk.E+tk.W, column=5, row=2)
+                self.btnConnect.grid(sticky=tk.E+tk.W, column=6, row=2)
+            else:
+                self.lblAddr.grid_forget()
+                self.inputLobby.grid_forget()
+                self.btnConnect.grid_forget()
+            self.address_toogle = state
