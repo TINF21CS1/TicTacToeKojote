ec05a334f9029eb3e5d806171a70c9823b36099a
Julian Lemmerich <41118534+JM-Lemmi@users.noreply.github.com>
2024-02-22 14:32:55 +0100

    add Lobby class refine websocket api definition remove player child classes
    Co-authored-by: Bengt Wegner <Petzys@users.noreply.github.com>

--- Server/player.py
+++ Server/player.py
@@ -1,4 +1,3 @@
-from statistics import Statistics
 import uuid
 
 class Player:
@@ -10,44 +9,8 @@ class Player:
         display_name (str): The name displayed for the player.
         statistics (Statistics): The statistics of the player.
     """
-    def __init__(self, id: int, display_name: str, statistics: Statistics):
+    def __init__(self, id: int, display_name: str, color: int):
         self._uuid: uuid.UUID = uuid.uuid4()
         self._id: int =  self._uuid.int
         self.display_name = display_name
-        self.statistics = statistics
-
-class RemotePlayer(Player):
-    """
-    Represents a remote player in the game.
-    
-    Attributes:
-        _id (int): The unique identifier of the player.
-        display_name (str): The name displayed for the player.
-        statistics (Statistics): The statistics of the player.
-    """
-    def __init__(self, id: int, display_name: str, statistics: Statistics):
-        super().__init__(id, display_name, statistics)
-
-class LocalPlayer(Player):
-    """
-    Represents a local player in the game.
-    
-    Attributes:
-        _id (int): The unique identifier of the player.
-        display_name (str): The name displayed for the player.
-        statistics (Statistics): The statistics of the player.
-    """
-    def __init__(self, id: int, display_name: str, statistics: Statistics):
-        super().__init__(id, display_name, statistics)
-
-class ComputerPlayer(Player):
-    """
-    Represents a computer player in the game.
-    
-    Attributes:
-        _id (int): The unique identifier of the player.
-        display_name (str): The name displayed for the player.
-        statistics (Statistics): The statistics of the player.
-    """
-    def __init__(self, id: int, display_name: str, statistics: Statistics):
-        super().__init__(id, display_name, statistics)
+        self.color = color
--- Server/websocket_server.py
+++ Server/websocket_server.py
@@ -1,4 +1,5 @@
 from game import Game
+from player import Player
 import asyncio
 import websockets
 import logging
@@ -6,32 +7,43 @@ import json
 
 logger = logging.getLogger()
 
-async def handler(websocket):
-    while True:
-        try:
-            message = await websocket.recv()
-        except websockets.ConnectionClosedOK:
-            logger.info("Connection Closed from Client-Side")
-            # TODO: Add handling when game is not over yet
-            break
+class Lobby:
+    def __init__(self, admin:Player) -> None:
+        self._players = [admin]
         
-        # TODO: Catch other errors for disconnects
 
-        logger.info(f"Received: {message}")
+    async def handler(self, websocket):
+        while True:
+            try:
+                message = await websocket.recv()
+            except websockets.ConnectionClosedOK:
+                logger.info("Connection Closed from Client-Side")
+                # TODO: Add handling when game is not over yet
+                break
+            
+            # TODO: Catch other errors for disconnects
+
+            logger.info(f"Received: {message}")
+            
+            message_json = json.loads(message)
+            match message_json["message_type"]:
+                case "lobby/join":
+                    Player(1, message_json["player"]["display_name"])
+                case "lobby/ready":
+                    pass
+                case "game/make_move":
+                    pass
+                case "chat/message":
+                    pass
+                case _:
+                    await websocket.send("Invalid Message Type")
         
-        message_json = json.loads(message)
-        match message_json["message_type"]:
-            case "join":
-                pass
-            case _:
-                await websocket.send("Invalid Message Type")
-    
 
-async def main():
-    async with websockets.serve(handler, "", 8001):
-        await asyncio.Future()  # run forever
+    async def start_server(self, port: int = 8765):
+        async with websockets.serve(self.handler, "", port):
+            await asyncio.Future()  # run forever
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    asyncio.run(Lobby.start_server(port = 8765, admin = Player(0, "admin")))
 
