40893e836e8912494d3aaa10d7e1147618e00743
bananabr3d <a.dubilyer@gmx.de>
2024-03-08 18:20:44 +0100

    fix: TTTK-19 profile_save.py: merged feat: TTT-19 profile_save.py: throws now errors if profile_color or profile_name is None, delete Functions added

--- Client/profile_save.py
+++ Client/profile_save.py
@@ -1,19 +1,21 @@
 import json
 from os.path import exists
 
-# Path to the profiles.json file
-path = ('../json_schema/profiles.json')
 
 class Profile:
     """
     This class is used to handle the profiles.json file. It is used to get, set, and add profiles to the file.
     """
+
+    def __init__(self, path='../json_schema/profiles.json'):
+        self.path = path
+
     def check_file(self):
         """
         This method checks if the file exists
         :return: True if the file exists, False if it does not
         """
-        if exists(path):
+        if exists(self.path):
             print("found")
             return True
         else:
@@ -26,12 +28,13 @@ class Profile:
         :return: An array of all profiles
         """
         if self.check_file():
-            with open(path, 'r') as file:
+            with open(self.path, 'r') as file:
                 data = json.load(file)
                 return data
         else:
             return None
-    def get_profile(self,profile_uuid):
+
+    def get_profile(self, profile_uuid):
         """
         This method returns a profile by its uuid
         :param profile_uuid:
@@ -39,7 +42,7 @@ class Profile:
         """
         if self.check_file():
             try:
-                with open(path, 'r') as file:
+                with open(self.path, 'r') as file:
                     data = json.load(file)
                     for profile in data:
                         if profile["profile_uuid"] == profile_uuid:
@@ -47,30 +50,34 @@ class Profile:
             except:
                 print("json error: Make sure profiles.json is formatted correctly")
         return None
-    def set_profile(self, profile_uuid , profile_name, profile_color):
+
+    def set_profile(self, profile_uuid, profile_name, profile_color):
         """
         This method sets the profile name and/or color by the uuid
         :param profile_uuid:
-        :param profile_name: *optional*
-        :param profile_color: *optional*
+        :param profile_name:
+        :param profile_color:
         """
-        if profile_name == None || profile_color == None:
+        if (profile_name or profile_color) == None:
             raise ValueError("name or color cannot be none")
-        
+
         if self.check_file():
             try:
-                with open(path, 'r+') as file:
+                with open(self.path, 'r+') as file:
                     data = json.load(file)
                     for profile in data:
                         if profile["profile_uuid"] == profile_uuid:
-                            profile["profile_name"] = profile_name
-                            profile["profile_color"] = profile_color
+                            if profile_name != None:
+                                profile["profile_name"] = profile_name
+                            if profile_color != None:
+                                profile["profile_color"] = profile_color
                             break
-                    with open(path, 'w') as file:
+                    with open(self.path, 'w') as file:
                         json.dump(data, file)
             except:
                 print("json error: Make sure profiles.json is formatted correctly")
         return None
+
     def get_profile_by_name(self, profile_name):
         if self.check_file():
             """
@@ -79,7 +86,7 @@ class Profile:
             :return: profile matching given name
             """
             try:
-                with open(path, 'r') as file:
+                with open(self.path, 'r') as file:
                     data = json.load(file)
                     for profile in data:
                         if profile["profile_name"] == profile_name:
@@ -98,7 +105,7 @@ class Profile:
         if self.check_file():
             entry = {"profile_name": profile_name, "profile_uuid": profile_uuid, "profile_color": profile_color}
             try:
-                with open(path, 'r+') as file:
+                with open(self.path, 'r+') as file:
                     data = json.load(file)
                     file.seek(0)
                     data.append(entry)
@@ -108,14 +115,62 @@ class Profile:
                 print("json error: Make sure profiles.json is formatted correctly")
 
         else:
-            with open(path, 'w') as file:
+            with open(self.path, 'w') as file:
                 entry = [{"profile_name": profile_name, "profile_uuid": profile_uuid, "profile_color": profile_color}]
                 json.dump(entry, file)
 
-#Testing
-#profile = Profile()
-#profile.add_new_profile("test", "test", "test")
-#print(profile.get_profiles())
-#print(profile.get_profile("test"))
-#profile.set_profile("test", "test2", "test3")
-#print(profile.get_profiles())
+    def delete_profile(self, profile_uuid):
+        """
+        This method deletes a profile by its uuid
+        :param profile_uuid:
+        """
+        if self.check_file():
+            try:
+                with open(self.path, 'r+') as file:
+                    data = json.load(file)
+                    for profile in data:
+                        if profile["profile_uuid"] == profile_uuid:
+                            data.remove(profile)
+                            break
+                    else:
+                        raise ValueError(f"Profile with given uuid: {profile_uuid} not found")
+                    with open(self.path, 'w') as file:
+                        json.dump(data, file)
+            except:
+                print("json error: Make sure profiles.json is formatted correctly")
+
+    def delete_profile_by_name(self, profile_name):
+        """
+        This method deletes a profile by its name
+        :param profile_name:
+        """
+        if self.check_file():
+            try:
+                with open(self.path, 'r+') as file:
+                    data = json.load(file)
+                    for profile in data:
+                        if profile["profile_name"] == profile_name:
+                            data.remove(profile)
+                            break
+                    else:
+                        raise ValueError(f"Profile with given name: {profile_name} not found")
+                    with open(self.path, 'w') as file:
+                        json.dump(data, file)
+            except:
+                print("json error: Make sure profiles.json is formatted correctly")
+
+    def delete_all_profiles(self):
+        """
+        This method deletes all profiles
+        """
+        if self.check_file():
+            with open(self.path, 'w') as file:
+                file.write("[]")
+
+# Testing
+# profile = Profile()
+# profile.add_new_profile("test", "test", "test")
+# print(profile.get_profiles())
+# print(profile.get_profile("test"))
+# profile.set_profile("test", "test2", "test3")
+# print(profile.get_profiles())
