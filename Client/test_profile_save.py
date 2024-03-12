d3306771de0ed966f6151b44f6039e057fd62d6a
bananabr3d <a.dubilyer@gmx.de>
2024-03-12 11:31:16 +0100

    feat: test_profile_save.py tests implemented

--- Client/test_profile_save.py
+++ Client/test_profile_save.py
@@ -1,6 +1,7 @@
 import unittest
 from profile_save import Profile
 
+#todo kda durch 0 teilen fixen
 
 class TestProfileSave(unittest.TestCase):
 
@@ -16,9 +17,31 @@ class TestProfileSave(unittest.TestCase):
         self.assertEqual(self.profile.get_profile_by_name("test"),
                          {"profile_name": "test", "profile_uuid": "test", "profile_color": "test"})
         self.profile.delete_all_profiles()
+        self.assertEqual(self.profile.get_profiles(), [])
 
     def test_get_profile_by_uuid(self):
         self.profile.add_new_profile("test", "test", "test")
         self.assertEqual(self.profile.get_profile("test"),
                          {"profile_name": "test", "profile_uuid": "test", "profile_color": "test"})
+        self.profile.delete_all_profiles()
+        self.assertEqual(self.profile.get_profiles(), [])
+    def test_add_new_profile(self):
+        self.profile.add_new_profile("test", "test", "test")
+        self.assertEqual(self.profile.get_profile("test"),
+                         {"profile_name": "test", "profile_uuid": "test", "profile_color": "test"})
+        self.profile.delete_all_profiles()
+        self.assertEqual(self.profile.get_profiles(), [])
+
+    def test_delete_profile(self):
+        self.profile.add_new_profile("test", "test", "test")
+        self.profile.add_new_profile("test2", "test", "test")
+        self.profile.delete_profile("test")
+        self.assertEqual(self.profile.get_profiles(), [{"profile_name": "test2", "profile_uuid": "test", "profile_color": "test"}])
+        self.profile.delete_all_profiles()
+
+    def test_delete_profile_by_name(self):
+        self.profile.add_new_profile("test", "test", "test")
+        self.profile.add_new_profile("test2", "test", "test")
+        self.profile.delete_profile_by_name("test")
+        self.assertEqual(self.profile.get_profiles(), [{"profile_name": "test2", "profile_uuid": "test", "profile_color": "test"}])
         self.profile.delete_all_profiles()
