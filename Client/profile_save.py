5b2fb92c2d3df0fa1fb4b7d8abf8e85a12e2e75e
bananabr3d <a.dubilyer@gmx.de>
2024-03-14 09:50:03 +0100

    fix: useless functions deleted, indentation error fixed

--- Client/profile_save.py
+++ Client/profile_save.py
@@ -3,6 +3,7 @@ from os.path import exists
 import os
 from Server.player import Player
 
+
 class Profile:
     """
     This class is used to handle the profiles.json file. It is used to get, set, and add profiles to the file.
@@ -26,34 +27,6 @@ class Profile:
         else:
             return []
 
-    def set_profile(self, player : Player):
-        """
-        This method sets the profile name and/or color by the uuid
-        :param profile_uuid:
-        :param profile_name:
-        :param profile_color:
-        """
-        player_dict = player.as_dict()
-        if (player_dict['display_name'] or player_dict['color']) == None:
-            raise ValueError("name or color cannot be none")
-
-        if self.check_file():
-            try:
-                with open(self.path, 'r+') as file:
-                    data = json.load(file)
-                    for profile in data:
-                        if profile["uuid"] == player_dict["uuid"]:
-                            if player_dict['display_name'] != None:
-                                profile["display_name"] = player_dict['display_name']
-                            if player_dict['color'] != None:
-                                profile["color"] = player_dict['color']
-                            break
-                    with open(self.path, 'w') as file:
-                        json.dump(data, file)
-            except:
-                raise RuntimeError("json error: Make sure profiles.json is formatted correctly")
-        return None
-
     def set_profiles(self, players: list):
         """
         This method sets the profile name and/or color by the uuid
@@ -62,32 +35,14 @@ class Profile:
         :param profile_color:
         """
 
-            try:
-                with open(self.path, 'w') as file:
-                    entry = []
-                    for player in players:
-                        entry.append(player.as_dict())
-                    json.dump(entry, file)
-            except:
-                raise RuntimeError("json error: Make sure profiles.json is formatted correctly")
-
-    def get_profile_by_name(self, player : Player):
-        if self.check_file():
-            """
-            This method returns a profile by its name
-            :param profile_name:
-            :return: profile matching given name
-            """
-            player_dict = player.as_dict()
-            try:
-                with open(self.path, 'r') as file:
-                    data = json.load(file)
-                    for profile in data:
-                        if profile["display_name"] == player_dict["display_name"]:
-                            return Player.from_dict(profile)
-            except:
-                print("json error: Make sure profiles.json is formatted correctly")
-        return None
+        try:
+            with open(self.path, 'w') as file:
+                entry = []
+                for player in players:
+                    entry.append(player.as_dict())
+                json.dump(entry, file)
+        except:
+            raise RuntimeError("json error: Make sure profiles.json is formatted correctly")
 
     def delete_all_profiles(self):
         """
@@ -96,4 +51,3 @@ class Profile:
         if exists(self.path):
             with open(self.path, 'w') as file:
                 file.write("[]")
-
